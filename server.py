91b6e2f269d857d2b9dc35763c900ab29bf70057
MrajaR <kingreivan77@gmail.com>
2024-11-15 04:37:29 +0700

    adding doctring to several function

--- server.py
+++ server.py
@@ -21,6 +21,12 @@ analyzer = DangerousGoodsAnalyzer()
 # Automatically assign a UUID to each new visitor
 @app.before_request
 def assign_user_uuid():
+    """
+    Automatically assigns a UUID to each new visitor. This is a before-request
+    handler that checks if a user_id is already in the session. If not, it
+    generates a new UUID and stores it in the session. This allows the same
+    UUID to be used across multiple requests from the same user.
+    """
     if 'user_id' not in session:
         # Generate and assign a new UUID for this session
         session['user_id'] = str(uuid.uuid4())
@@ -29,6 +35,12 @@ def assign_user_uuid():
 @app.route('/')
 def index():
     # Get the UUID of the current user from the session
+    """
+    Displays the main page of the web application, which allows users to upload a PDF
+    file and have it analyzed by the LLMRAG model.
+
+    :return: The rendered HTML template for the main page
+    """
     user_uuid = session.get('user_id')
     print(f"Current user UUID: {user_uuid}")
     
@@ -36,6 +48,12 @@ def index():
 
 @app.route('/analyze-msds', methods=['POST'])
 def analyze_msds():
+    """
+    Endpoint to analyze a Material Safety Data Sheet (MSDS) document and return the
+    relevant results in HTML format.
+
+    :return: A JSON response containing the HTML content of the analysis
+    """
     relevant_text = analyzer.get_relevant_chunks()
     print(relevant_text)
     llm_response = analyzer.get_llm_response(relevant_text)
@@ -48,6 +66,17 @@ def analyze_msds():
 
 @app.route('/process-msds', methods=['POST'])
 def process_uploaded_msds():
+    """
+    Handles the uploading and processing of a Material Safety Data Sheet (MSDS) PDF file.
+
+    This endpoint expects a PDF file to be uploaded as part of a multipart/form-data request. The file is saved
+    to a user-specific directory and processed using the DangerousGoodsAnalyzer. The user is identified by a
+    unique session-based UUID.
+
+    Returns:
+        Response: A JSON response indicating the success or failure of the file upload and processing.
+                  If the PDF file is not included in the request, an error response is returned.
+    """
     user_id = session['user_id']
 
     if 'pdf' not in request.files:
